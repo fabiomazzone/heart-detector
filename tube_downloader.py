1a7b031223f4861de6445fe390777f52e3c58036
Fabio Mazzone <fabio.mazzone@me.com>
2022-06-20 14:32:32 +0200

    [feat]: extract video frames

--- tube_downloader.py
+++ tube_downloader.py
@@ -1,5 +1,10 @@
 from tqdm import tqdm
 from pytube import YouTube
+import os
+from os.path import isfile, join
+import cv2
+from cv2 import VideoCapture
+from datetime import datetime
 
 videos = ['https://youtu.be/HorTS-WNNBk', 'https://youtu.be/Mdx388nfzCY',
           'https://youtu.be/51aaWhfnzNQ', 'https://youtu.be/SS4ntXD10r8',
@@ -17,5 +22,33 @@ def download_videos(video_urls, path='videos'):
             download(output_path=path)
 
 
+def extract_frames(source_path='videos',
+                   dest_path='frames',
+                   filename_pattern='{dest}_{date}_{framestamp:.3f}.jpg'):
+    source_files = [file for file in os.listdir(
+        source_path) if isfile(join(source_path, file))]
+
+    current_date = datetime.now().strftime('%Y%m%d')
+
+    os.makedirs(dest_path)
+
+    for file in source_files:
+        video = VideoCapture(join(source_path, file))
+        file_destination = join(dest_path, file)
+        ret, frame = video.read()
+        while(video.isOpened() and ret):
+            timestamp = video.get(cv2.CAP_PROP_POS_MSEC)
+            filename = filename_pattern.format(
+                dest=file_destination,
+                date=current_date,
+                framestamp=round(timestamp, 3))
+
+            cv2.imwrite(filename, frame)
+            ret, frame = video.read()
+
+        video.release()
+
+
 if __name__ == "__main__":
-    download_videos(videos)
+    # download_videos(videos)
+    extract_frames()
